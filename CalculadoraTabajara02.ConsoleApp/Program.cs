54cb0c0b59177295ed024b7f940aea42cd04ff43
Bruno <Bruno@DESKTOP-MCC68NG>
2023-06-07 15:38:27 -0300

    Implementado Menu Principal

--- CalculadoraTabajara02.ConsoleApp/Program.cs
+++ CalculadoraTabajara02.ConsoleApp/Program.cs
@@ -4,7 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string opcao;
+
+            List<String> operacoes = new List<string>();
+
+            while (true)
+            {
+                #region Menu Principal
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara 1.0\n");
+
+                Console.WriteLine("Tela Principal\n");
+
+                Console.WriteLine("Digite 1 para Adição\n");
+
+                Console.WriteLine("Digite 2 para Subtração\n");
+
+                Console.WriteLine("Digite 3 para Multiplicação\n");
+
+                Console.WriteLine("Digite 4 para Divisão\n");
+
+                Console.WriteLine("Digite 5 para Visualizar as operações\n");
+
+                Console.WriteLine("Digite S para sair\n");
+
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+
+                if (opcao.Equals("s", StringComparison.OrdinalIgnoreCase))
+                    break;
+
+                if (opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4" && opcao != "5")
+                {
+                    Console.WriteLine("Você precisa digitar uma opção válida!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                #endregion
+            }
         }
     }
 }
